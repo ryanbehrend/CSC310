42ebefa5026a6cd8a365825515bfa5a7119a1b18
ryanbehrend <ryanjbehrend@gmail.com>
2026-03-24 19:01:55 -0500

    removed delete functionality

--- A3/mainLLRB.cpp
+++ A3/mainLLRB.cpp
@@ -3,20 +3,12 @@
 int main() {
     LLRBTREE rb;
 
-    for (int i = 1; i <= 30; i++)
+    for (int i = 0; i <= 30; i++)
     {
         rb.insert(i);
     }
 
     cout << "Constructed RB tree:\n";
-    rb.printTree();
-
-    for (int i = 1; i <= 30; i += 5)
-    {
-        rb.remove(i);
-    }
-
-    cout << "Removed some elements:\n";
     rb.printTree();
 
     cout << "Total rotation count: " << rb.rotationCount << endl;
