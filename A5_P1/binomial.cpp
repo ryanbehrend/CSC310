31410b7c7133e7fc690927bf9c993e4e77944847
Ryan <ryanjbehrend@gmail.com>
2026-04-14 13:02:39 -0500

    brought our in class implementaation for all functions except for deletion/decreasing

--- A5_P1/binomial.cpp
+++ A5_P1/binomial.cpp
@@ -2,69 +2,207 @@
 
 binomialNode::binomialNode(int k)
 {
-
+    key = k;
+    degree = 0;
+    sibling = nullptr;
+    child = nullptr;
+    parent = nullptr;
 }
 
-// ---------------------- PRIVATE FUNCTIONS ------------------------------
-binomialNode* BinomialHeap::unionHeap(binomialNode* heap1, binomialNode* heap2)
+// ---------------------- PRIVATE FUNCTIONS ------------------------------ //
+
+binomialNode *BinomialHeap::unionHeap(binomialNode *heap1, binomialNode *heap2)
 {
+    if (!heap1)
+    {
+        return heap2;
+    }
+    if (!heap2)
+    {
+        return heap1;
+    }
 
+    binomialNode *newHead = nullptr;
+    binomialNode **pos = &newHead;
+
+    while (heap1 && heap2)
+    {
+        if (heap1->degree <= heap2->degree)
+        {
+            *pos = heap1;
+            heap1 = heap1->sibling;
+        }
+        else
+        {
+            *pos = heap2;
+            heap2 = heap2->sibling;
+        }
+
+        pos = &((*pos)->sibling);
+    }
+
+    *pos = (heap1) ? heap1 : heap2;
+
+    return newHead;
 }
 
-
-binomialNode* BinomialHeap::mergeTrees(binomialNode* tree1, binomialNode* tree2)
+binomialNode *BinomialHeap::mergeTrees(binomialNode *tree1, binomialNode *tree2)
 {
+    if (tree1->key > tree2->key)
+    {
+        swap(tree1, tree2);
+    }
 
+    tree2->parent = tree1;
+    tree2->sibling = tree1->child;
+    tree1->child = tree2;
+    tree1->degree++;
+
+    return tree1;
 }
 
-void BinomialHeap::linkTrees(binomialNode*& prev, binomialNode*& cur, binomialNode*& next)
+void BinomialHeap::linkTrees(binomialNode *&prev, binomialNode *&curr, binomialNode *&next)
 {
+    if (curr->degree != next->degree || next->sibling && next->sibling->degree == curr->degree)
+    {
+        prev = curr;
+        curr = next;
+    }
+    else
+    {
+        if (curr->key <= next->key)
+        {
+            curr->sibling = next->sibling;
+            mergeTrees(curr, next);
+        }
+        else
+        {
+            if (!prev)
+            {
+                head = next;
+            }
+            else
+            {
+                prev->sibling = next;
+            }
 
+            mergeTrees(next, curr);
+            curr = next;
+        }
+    }
 }
 
-// ---------------- PUBLIC FUNCTIONS ------------------------------
+// ---------------- PUBLIC FUNCTIONS ------------------------------ //
+
 BinomialHeap::BinomialHeap()
 {
-
+    head = nullptr;
 }
 
 void BinomialHeap::insert(int key)
 {
+    BinomialHeap tempHeap;
+    tempHeap.head = new binomialNode(key);
 
+    head = unionHeap(head, tempHeap.head);
+
+    if (!head || !head->sibling)
+    {
+        return;
+    }
+
+    binomialNode *prev = nullptr;
+    binomialNode *curr = head;
+    binomialNode *next = curr->sibling;
+
+    while (next)
+    {
+        linkTrees(prev, curr, next);
+        next = curr->sibling;
+    }
 }
 
-void BinomialHeap::merge(BinomialHeap& other)
+void BinomialHeap::merge(BinomialHeap &other)
 {
+    head = unionHeap(head, other.head);
 
+    if (!head || !head->sibling)
+    {
+        return;
+    }
+
+    binomialNode *prev = nullptr;
+    binomialNode *curr = head;
+    binomialNode *next = curr->sibling;
+
+    while (next)
+    {
+        linkTrees(prev, curr, next);
+        next = curr->sibling;
+    }
 }
 
 int BinomialHeap::findMin()
 {
+    if (!head)
+    {
+        return -1;
+    }
 
+    binomialNode *minNode = head;
+    int minKey = head->key;
+
+    binomialNode *temp = head->sibling;
+    while (temp)
+    {
+        if (temp->key < minKey)
+        {
+            minNode = temp;
+            minKey = temp->key;
+        }
+
+        temp = temp->sibling;
+    }
+
+    return minKey;
 }
 
-void BinomialHeap::printHeap() {
+int BinomialHeap::deleteMin()
+{
+}
+
+bool BinomialHeap::decreaseKey(int oldKey, int newKey)
+{
+}
+
+bool BinomialHeap::deleteKey(int key)
+{
+}
+
+void BinomialHeap::printHeap()
+{
     cout << "Binomial Heap:\n";
-    binomialNode* current = head;
+    binomialNode *current = head;
 
-
-    while (current != nullptr) {
-        cout << endl << "Tree of degree " << current->degree << endl;
+    while (current != nullptr)
+    {
+        cout << endl
+             << "Tree of degree " << current->degree << endl;
         printTree(current, 0);
         current = current->sibling;
     }
-
 }
 
-void BinomialHeap::printTree(binomialNode* node, int space) 
+void BinomialHeap::printTree(binomialNode *node, int space)
 {
-    if (node == nullptr) 
+    if (node == nullptr)
         return;
 
     cout << setw(space * 2) << node->key << endl;
 
-    binomialNode* child = node->child;
-    while (child) {
+    binomialNode *child = node->child;
+    while (child)
+    {
         printTree(child, space + 3);
         child = child->sibling;
     }
--- A5_P1/binomial.h
+++ A5_P1/binomial.h
@@ -1,6 +1,8 @@
 #ifndef BINOMIAL_HEAP_H
 #define BINOMIAL_HEAP_H
 
+#include <customErrorClass.h>
+
 #include <iostream>
 #include <iomanip>
 
@@ -10,35 +12,35 @@ struct binomialNode
 {
     int key;
     int degree;
-    binomialNode* sibling;
-    binomialNode* child;
-    binomialNode* parent;
+    binomialNode *sibling;
+    binomialNode *child;
+    binomialNode *parent;
 
     binomialNode(int k);
 };
 
 class BinomialHeap
 {
-    private:
-        binomialNode* head;
+private:
+    binomialNode *head;
 
-        binomialNode* unionHeap(binomialNode* heap1, binomialNode* heap2);
-        binomialNode* mergeTrees(binomialNode* tree1, binomialNode* tree2);
-        void linkTrees(binomialNode*& prev, binomialNode*& cur, binomialNode*& next);
+    binomialNode *unionHeap(binomialNode *heap1, binomialNode *heap2);
+    binomialNode *mergeTrees(binomialNode *tree1, binomialNode *tree2);
+    void linkTrees(binomialNode *&prev, binomialNode *&cur, binomialNode *&next);
 
-    public:
-        BinomialHeap();
+public:
+    BinomialHeap();
 
-        void insert(int key);
-        void merge(BinomialHeap& other);
-        int findMin();
+    void insert(int key);
+    void merge(BinomialHeap &other);
+    int findMin();
 
-        //deleteMin()
-        //decreaseKey(oldKey, newKey);
-        //deleteKey(int key);
+    int deleteMin();
+    bool decreaseKey(int oldKey, int newKey);
+    bool deleteKey(int key);
 
-        void printHeap();
-        void printTree(binomialNode* node, int space);
+    void printHeap();
+    void printTree(binomialNode *node, int space);
 };
 
 #endif
